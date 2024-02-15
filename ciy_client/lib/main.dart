5488c4e16b9869f05896fe94892e19ae809b8ab4
ronen4822 <ronen4822@gmail.com>
2024-02-15 08:28:57 +0200

    -give the stuff

--- ciy_client/lib/main.dart
+++ ciy_client/lib/main.dart
@@ -2,6 +2,7 @@ import 'package:ciy_client/globals/vm_characteristics.dart';
 import 'package:ciy_client/pages/settings/main_page/unified_page.dart';
 import 'package:flutter/material.dart';
 import 'package:window_manager/window_manager.dart';
+import 'package:flex_color_scheme/flex_color_scheme.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -33,10 +34,7 @@ class CloudIY extends StatelessWidget {
     return MaterialApp(
       title: 'CloudIY Client',
       debugShowCheckedModeBanner: false,
-      theme: ThemeData(
-          useMaterial3: true,
-          colorScheme: ColorScheme.fromSeed(
-              seedColor: Color.fromARGB(255, 166, 207, 241), brightness: Brightness.dark)),
+      theme: FlexThemeData.dark(scheme: FlexScheme.blue),
       home: UnifiedPage(),
     );
   }
