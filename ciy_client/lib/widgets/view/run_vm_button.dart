19ee51413c6b65a58940c533a690cc1f98880dbe
ronen4822 <ronen4822@gmail.com>
2024-02-15 14:58:38 +0200

    -do better stuff

--- ciy_client/lib/widgets/view/run_vm_button.dart
+++ ciy_client/lib/widgets/view/run_vm_button.dart
@@ -1,4 +1,5 @@
 import 'package:ciy_client/widgets/bloc/launch_vm_bloc.dart';
+import 'package:ciy_client/widgets/bloc/vm_installation_bloc.dart';
 import 'package:ciy_client/widgets/events/vm_running_events.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
@@ -6,22 +7,27 @@ import 'package:flutter_bloc/flutter_bloc.dart';
 class RunVMButton extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return BlocBuilder<VMRunBloc, CurrentVMState>(builder: (context, state) {
-      String buttonText = "Run";
-      bool enabled = true;
-      VMRuntimeEvent? event;
+    return Builder(builder: (context) {
+      final vmRunState = context.watch<VMRunBloc>().state;
+      final vmInstallState = context.watch<VMInstallationBloc>().state;
 
-      context.select((VMRunBloc bloc) {
-        if (bloc.state.running == RunningState.running) {
-          buttonText = "Terminate";
-          event = VMTerminateRequest();
-        } else if (bloc.state.running == RunningState.inProgress) {
-          buttonText = "Initializing";
-          enabled = false;
-        } else {
-          event = VMStartEvent();
-        }
-      });
+      String buttonText = "Run";
+      bool enabled =
+          vmInstallState.backendInstalled == InstallationStatus.success &&
+              vmInstallState.virtualizationInstalled ==
+                  InstallationStatus.success &&
+              vmInstallState.vmInstalled == InstallationStatus.success;
+
+      VMRuntimeEvent? event;
+      if (vmRunState.running == RunningState.running) {
+        buttonText = "Terminate";
+        event = VMTerminateRequest();
+      } else if (vmRunState.running == RunningState.inProgress) {
+        buttonText = "Initializing";
+        enabled = false;
+      } else {
+        event = VMStartEvent();
+      }
 
       return Padding(
           padding: const EdgeInsets.all(8.0),
