5bd06438f71c7896a22e3ae7fbf8eefc767c6ba1
ronen4822 <ronen4822@gmail.com>
2024-02-26 07:35:32 +0200

    -im stupid

--- github_windows_setup_flutter.ps1
+++ github_windows_setup_flutter.ps1
@@ -1,73 +1,77 @@
-#!/bin/bash
-
 # Options
-ARCH=$(echo "${RUNNER_ARCH:-x64}" | awk '{print tolower($0)}')
-OS=$(echo "${RUNNER_OS:-linux}" | awk '{print tolower($0)}')
+$ARCH = ($env:RUNNER_ARCH -split ':')[0].ToLower()
+$OS = ($env:RUNNER_OS -split ':')[0].ToLower()
 
 # Args
-FLUTTER_VERSION=${1:-3.0.2}
-FLUTTER_CHANNEL=${2:-stable}
-FLUTTER_OS=$OS
+$FLUTTER_VERSION = $args[0]
+if (-not $FLUTTER_VERSION) { $FLUTTER_VERSION = "3.0.2" }
+$FLUTTER_CHANNEL = $args[1]
+if (-not $FLUTTER_CHANNEL) { $FLUTTER_CHANNEL = "stable" }
+$FLUTTER_OS = $OS
 
 # Detect the latest version
-if [[ $FLUTTER_VERSION == "latest" ]]
-then
-        echo "Detecting latest version..."
-        curl -L https://storage.googleapis.com/flutter_infra_release/releases/releases_$OS.json -o "${RUNNER_TEMP}/flutter_release.json"
-        CURRENT_RELEASE=$(jq -r ".current_release.${FLUTTER_CHANNEL}" "${RUNNER_TEMP}/flutter_release.json")
-        FLUTTER_VERSION=$(jq -r ".releases | map(select(.hash == \"${CURRENT_RELEASE}\")) | .[0].version" "${RUNNER_TEMP}/flutter_release.json")
-        rm "${RUNNER_TEMP}/flutter_release.json"
-fi
+if ($FLUTTER_VERSION -eq "latest") {
+    Write-Host "Detecting latest version..."
+    Invoke-WebRequest -Uri "https://storage.googleapis.com/flutter_infra_release/releases/releases_$OS.json" -OutFile "$env:RUNNER_TEMP/flutter_release.json"
+    $CURRENT_RELEASE = (Get-Content "$env:RUNNER_TEMP/flutter_release.json" | ConvertFrom-Json).current_release.$FLUTTER_CHANNEL
+    $FLUTTER_VERSION = (Get-Content "$env:RUNNER_TEMP/flutter_release.json" | ConvertFrom-Json).releases | Where-Object { $_.hash -eq $CURRENT_RELEASE } | Select-Object -ExpandProperty version
+    Remove-Item "$env:RUNNER_TEMP/flutter_release.json"
+}
 
 # OS archive file extension
-EXT="zip"
-if [[ $OS == linux ]]
-then
-	EXT="tar.xz"
-fi
+$EXT = "zip"
+if ($OS -eq "linux") {
+    $EXT = "tar.xz"
+}
 
 # Apple Intel or Apple Silicon
-if [[ $OS == macos && $ARCH == 'arm64' && $FLUTTER_VERSION == 3.* ]]
-then
-	FLUTTER_OS="${FLUTTER_OS}_$ARCH"
-fi
+if ($OS -eq "macos" -and $ARCH -eq 'arm64' -and $FLUTTER_VERSION -like "3.*") {
+    $FLUTTER_OS = "${FLUTTER_OS}_$ARCH"
+}
 
-FLUTTER_RUNNER_TOOL_CACHE="${RUNNER_TOOL_CACHE}/flutter-${RUNNER_OS}-${FLUTTER_VERSION}-${RUNNER_ARCH}"
+$FLUTTER_RUNNER_TOOL_CACHE = "$env:RUNNER_TOOL_CACHE/flutter-$env:RUNNER_OS-$FLUTTER_VERSION-$env:RUNNER_ARCH"
 
-# https://storage.googleapis.com/flutter_infra_release/releases/stable/macos/flutter_macos_arm64_3.0.2-stable.zip
-FLUTTER_RELEASE_URL="https://storage.googleapis.com/flutter_infra_release/releases"
+$FLUTTER_RELEASE_URL = "https://storage.googleapis.com/flutter_infra_release/releases"
 
+if (-not (Test-Path $FLUTTER_RUNNER_TOOL_CACHE)) {
+    Write-Host "Installing Flutter SDK version \"$FLUTTER_VERSION\" from the $FLUTTER_CHANNEL channel on $FLUTTER_OS"
+    $FLUTTER_BUILD = "flutter_${FLUTTER_OS}_${FLUTTER_VERSION}-${FLUTTER_CHANNEL}.$EXT"
+    $FLUTTER_DOWNLOAD_URL = "$FLUTTER_RELEASE_URL/$FLUTTER_CHANNEL/$FLUTTER_OS/$FLUTTER_BUILD"
 
-if [ ! -d "${FLUTTER_RUNNER_TOOL_CACHE}" ]; then
-	echo "Installing Flutter SDK version \"${FLUTTER_VERSION}\" from the ${FLUTTER_CHANNEL} channel on ${FLUTTER_OS}"
-	FLUTTER_BUILD="flutter_${FLUTTER_OS}_${FLUTTER_VERSION}-${FLUTTER_CHANNEL}.${EXT}"
-	FLUTTER_DOWNLOAD_URL="${FLUTTER_RELEASE_URL}/${FLUTTER_CHANNEL}/${FLUTTER_OS}/${FLUTTER_BUILD}"
+    Write-Host "Downloading $FLUTTER_DOWNLOAD_URL"
 
-	echo "Downloading ${FLUTTER_DOWNLOAD_URL}"
+    $DOWNLOAD_PATH = "$env:TEMP\$FLUTTER_BUILD"
+    Invoke-WebRequest -Uri $FLUTTER_DOWNLOAD_URL -OutFile $DOWNLOAD_PATH
 
-	DOWNLOAD_PATH="${FLUTTER_RUNNER_TOOL_CACHE}/${FLUTTER_BUILD}"
-	curl --connect-timeout 15 --retry 5 "$FLUTTER_DOWNLOAD_URL" > ${DOWNLOAD_PATH}
+    Write-Host "Finished downloading $FLUTTER_DOWNLOAD_URL"
 
-	mkdir -p "${FLUTTER_RUNNER_TOOL_CACHE}"
+    New-Item -ItemType Directory -Path $FLUTTER_RUNNER_TOOL_CACHE -ErrorAction SilentlyContinue | Out-Null
 
-	tar -C "${FLUTTER_RUNNER_TOOL_CACHE}" -xf ${DOWNLOAD_PATH}
+    if ($OS -eq "linux") {
+        tar -C $FLUTTER_RUNNER_TOOL_CACHE -xf $DOWNLOAD_PATH > $null
+    }
+    else {
+        Write-Host "Unzipping $FLUTTER_DOWNLOAD_URL"
+        Expand-Archive -Path $DOWNLOAD_PATH -DestinationPath $FLUTTER_RUNNER_TOOL_CACHE
+    }
 
-	if [ $? -ne 0 ]; then
-		echo -e "::error::Download failed! Please check passed arguments."
-		exit 1
-	fi
-else
-	echo "Cache restored Flutter SDK version \"${FLUTTER_VERSION}\" from the ${FLUTTER_CHANNEL} channel on ${FLUTTER_OS}"
-fi
+    if (-not $?) {
+        Write-Host "::error::Download failed! Please check passed arguments."
+        exit 1
+    }
+}
+else {
+    Write-Host "Cache restored Flutter SDK version \"$FLUTTER_VERSION\" from the $FLUTTER_CHANNEL channel on $FLUTTER_OS"
+}
 
 # Configure pub to use a fixed location.
-echo "PUB_CACHE=${HOME}/.pub-cache" >> $GITHUB_ENV
+Add-Content -Path $env:GITHUB_ENV -Value "PUB_CACHE=$env:HOME/.pub-cache"
 
 # Update paths.
-echo "${HOME}/.pub-cache/bin" >> $GITHUB_PATH
-echo "${FLUTTER_RUNNER_TOOL_CACHE}/flutter/bin" >> $GITHUB_PATH
+Add-Content -Path $env:GITHUB_PATH -Value "$env:HOME/.pub-cache/bin"
+Add-Content -Path $env:GITHUB_PATH -Value "$FLUTTER_RUNNER_TOOL_CACHE/flutter/bin"
 
 # Report success, and print version.
-echo -e "Succesfully installed Flutter SDK:"
-${FLUTTER_RUNNER_TOOL_CACHE}/flutter/bin/dart --version
-${FLUTTER_RUNNER_TOOL_CACHE}/flutter/bin/flutter --version
+Write-Host "Succesfully installed Flutter SDK:"
+& "$FLUTTER_RUNNER_TOOL_CACHE/flutter/bin/dart" --version
+& "$FLUTTER_RUNNER_TOOL_CACHE/flutter/bin/flutter" --version
